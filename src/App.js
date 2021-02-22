9ef99e79ade0a9d234aa12bf6abf18ca2f721d51
Hamida Sh <sharifovahamida@gmail.com>
2021-02-21 22:05:10 -0800

    Auth and BubblePage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import BubblePage from "./components/BubblePage";
 import Login from "./components/Login";
+import PrivateRoute from "./components/PrivateRoute";
 import "./styles.scss";
+
+
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
+        <PrivateRoute path="/bubbles" component={BubblePage}/>
       </div>
     </Router>
   );
--- src/helpers/axiosWithAuth.js
+++ src/helpers/axiosWithAuth.js
@@ -3,4 +3,12 @@ import axios from "axios";
 //Task List:
 //Build and export a function used to send in our authorization token
 
-//first commit
+export const axiosWithAuth = () => {
+  const token = localStorage.getItem("token");
+  return axios.create({
+    headers: {
+      authorization: token,
+    },
+    baseURL: "http://localhost:5000/api",
+  });
+};
